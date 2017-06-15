28b9618bf2858eab19306f79fed3194397dcc604
zeroflux <pnazarov@at-consulting.ru>
2017-06-15 17:21:59 +0300

    added uniparser lib

---
+++ lib/parser.rb
@@ -0,0 +1,22 @@
+require __dir__ + '/uniparser.rb'
+
+def doParse
+	if File.exist?("/etc/atceph/config")
+		parser = UniParser.new("/etc/atceph/config")
+	else
+		#parser = UniParser.new( __dir__ + "/config" )
+		parser = UniParser.new("/tmp/config" )
+	end
+
+	parser.parse_all()
+	parser.parse_cli( ["-h", "--help"], "Show this help", false)
+	parser.parse_cli( ["-c", "--config"], "Use this ceph configuration file instead of the default", true, [:ceph, :config])
+	parser.parse_cli( ["-n", "--name"], "Client name for authentication", true, [:ceph, :client])
+	parser.parse_cli( ["-f", "--format"], "Change output format")
+
+	ceph_args = ""
+	ceph_args = "#{ceph_args} -c #{parser.merged[:ceph][:config]}" if parser.merged.dig(:ceph, :config)
+	ceph_args = "#{ceph_args} --name #{parser.merged[:ceph][:client]}" if parser.merged.dig(:ceph, :client)
+
+	return parser, ceph_args
+end
