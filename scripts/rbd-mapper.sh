c662e708c84e253672269b3deaed68f944eb5d07
zeroflux <pnazarov@at-consulting.ru>
2017-06-16 04:07:35 +0300

    stat_file support and minor refactor

--- scripts/rbd-mapper.sh
+++ scripts/rbd-mapper.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-MAP_FILE="/etc/atceph/rbdmap"
+
+if [ -f "/etc/atceph/config" ]; then
+      conf="/etc/atceph/config"
+else
+      conf="/etc/atceph/config"
+      pushd `dirname $0` > /dev/null
+      RUNDIR=`pwd`
+      WORKDIR="$(dirname ${RUNDIR})"
+      conf="${WORKDIR}/config"
+fi
+
+MAP_FILE=`grep map_file ${conf} | awk -F'=' '{print $2}' | sed 's/"//g' | tr -d '[:space:]'`
+if [ $MAP_FILE == "scripts/rbdmap" ]; then MAP_FILE="${RUNDIR}/rbdmap"; fi
+CLIENT=`grep client ${conf} | awk -F'=' '{print $2}' | sed 's/"//g' | tr -d '[:space:]'`
+CONFIG=`grep config ${conf} | awk -F'=' '{print $2}' | sed 's/"//g' | tr -d '[:space:]'`
 
 while read DEV PARAMS; do
       case "$DEV" in
       ""|\#*)
             continue
             ;;
-      */*)
-            ;;
-      *)
-            DEV=rbd/$DEV
-            ;;
       esac
 
-      OIFS=$IFS
-      IFS=','
-      for PARAM in ${PARAMS[@]}; do
-            CMDPARAMS="$CMDPARAMS --$(echo $PARAM | tr '=' ' ')"
-      done
-
-      IFS=$OIFS
       if [ ! -b /dev/rbd/$DEV ]; then
-            echo "rbd map $DEV $CMDPARAMS"
-            rbd map $DEV $CMDPARAMS
+            echo "rbd map -c ${CONFIG} -n ${CLIENT} $DEV"
+            rbd map -c ${CONFIG} -n ${CLIENT} $DEV
       fi
 done < $MAP_FILE
